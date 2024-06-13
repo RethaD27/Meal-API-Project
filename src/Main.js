978d06c737c213f65d6cb7e3bac43a31aa5fd15f
Rethabile Diale <rethadiale27@gmail.com>
2024-06-13 21:40:46 +0200

    Added styling on cards, dark mode toggle

--- src/Main.js
+++ src/Main.js
@@ -1,35 +1,67 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
+import axios from "axios";
 import "./style.css";
 
+function MealDetails({ meal }) {
+  return (
+    <div className="meal-details">
+      <h2>{meal.strMeal}</h2>
+      <img src={meal.strMealThumb} alt={meal.strMeal} />
+      <p>{meal.strInstructions}</p>
+      {/* Add ingredients and other details */}
+    </div>
+  );
+}
+
 function Main() {
-  const [items, setitems] = useState([]);
+  const [items, setItems] = useState([]);
+  const [selectedMeal, setSelectedMeal] = useState(null);
 
   useEffect(() => {
     axios
       .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
       .then((res) => {
-        // console.log(res.data);
-        setitems(res.data.meals);
+        setItems(res.data.meals);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  const itemslist = items.map(({ strMeal, strMealThumb, idMeal }) => {
-    return (
-      <section className="card">
-        <img src={strMealThumb} />
-        <section className="content">
-          <p>{strMeal}</p>
-          <p>#{idMeal}</p>
-        </section>
-      </section>
-    );
-  });
+  const handleMealClick = (idMeal) => {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+      .then((res) => {
+        setSelectedMeal(res.data.meals[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  return <div className="items-container">{itemslist}</div>;
+  return (
+    <div>
+      {selectedMeal ? (
+        <MealDetails meal={selectedMeal} />
+      ) : (
+        <div className="items-container">
+          {items.map(({ strMeal, strMealThumb, idMeal }) => (
+            <section
+              key={idMeal}
+              className="card"
+              onClick={() => handleMealClick(idMeal)}
+            >
+              <img src={strMealThumb} alt={strMeal} />
+              <section className="content">
+                <p>{strMeal}</p>
+                <p>#{idMeal}</p>
+              </section>
+            </section>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Main;
